42c0764a9678999b91342c18e61abd9230512cfa
Marcus Cheung <kings88819@gmail.com>
2020-11-23 15:38:17 +0800

    Update real_test.py

--- real_test.py
+++ real_test.py
@@ -3,11 +3,12 @@ import numpy as np
 import cv2
 import pandas as pd
 
-imgs = ['0001.jpg', '0002.jpg', '0003.jpg', '0004.jpg', '0005.jpg', '0006.jpg', '0007.jpg', '0008.jpg', '0009.jpg', '0010.jpg']
-imgs = [cv2.imread('train/{}'.format(x)) for x in imgs]
+imgs = pd.read_csv('dataset_large.txt',header=None)
+imgs = imgs.iloc[:,0]
+imgs = imgs.tolist()
+imgs = [cv2.imread(x) for x in imgs]
 imgs = [cv2.resize(x, (224,224)) for x in imgs]
 imgs = [cv2.cvtColor(x, cv2.COLOR_BGR2RGB) for x in imgs]
-imgs = np.array(imgs).astype(np.uint8)
 
 rknn = RKNN()
 rknn.load_rknn('b0_model_lite.rknn')
@@ -30,8 +31,9 @@ for img in imgs:
 print('done')
 test_frame = pd.DataFrame(np.array(result))
 print(test_frame)
-real_frame = pd.read_csv('train/train.csv', header=None)
-print(real_frame)
 
-
+with open('output.txt', 'w') as f:
+    for name in result:
+        f.write('{}\n'.format(name))
+       
 rknn.release()
